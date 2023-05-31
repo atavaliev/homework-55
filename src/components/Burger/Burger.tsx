9fd148973901793fecebd05fd53bea0eb9d0ba1d
Atavaliev Timur <tatavaliev@gmail.com>
2023-05-31 17:27:43 +0600

    Render Burger Dynamically from initial state

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -14,14 +14,18 @@ const Burger: React.FC<TProps> = ({ingredientList}) => {
     }
     return (
         <div className="Burger">
+
             <div className="BreadTop">
                 <div className="Seeds1"></div>
                 <div className="Seeds2"></div>
             </div>
-            <div className="Cheese"></div>
-            <div className="Bacon"></div>
-            <div className="Meat"></div>
-            <div className="Salad"></div>
+            <>
+                {
+                    ingredientList.map(i => {
+                     return  (<div className={i.name}/>)
+                    })
+                }
+            </>
             <div className="BreadBottom"></div>
         </div>
     );
